735e00978fdbaacf87c3b08a8eac032fb660c1ba
chkalyan <you@example.com>
2024-06-18 16:08:32 +0530

    committing and code to localrepository

---
+++ addition.js
@@ -0,0 +1,12 @@
+const num1 = 5;
+const num2 = 10;
+const sum = num1 + num2;
+console.log(sum); 
+
+
+// var a,b,c;
+// a=Number(prompt("value a:"));
+// b=Number(prompt("value b:"));
+// c=a+b;
+// document.write("sum of a and b"+c);
+
---
+++ greaternumber.js
@@ -0,0 +1,7 @@
+const num1 =20;
+const num2 =30;
+ if(num1>num2){
+    console.log(num1 + " is greater than "+num2);
+ }else{
+    console.log(num2 + " is greater than "+num1);
+ }
